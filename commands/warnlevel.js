eddf2d152e32098e41c75275b575a01c4351b8fe
GiCl90 <65637325+GiCl90@users.noreply.github.com>
2020-05-24 20:06:40 +0200

    Update warnlevel.js

--- commands/warnlevel.js
+++ commands/warnlevel.js
@@ -4,7 +4,7 @@ const ms = require("ms");
 const customisation = require('../customisation.json');
 
 exports.run = (client, message, args) => {
-    let warns = JSON.parse(fs.readFileSync("../warnings.json", "utf8"));
+    let warns = JSON.parse(fs.readFileSync("./warnings.json", "utf8"));
     let user = message.mentions.users.first();
     if(message.mentions.users.size < 1) return message.reply('You must mention someone to check their warns.').catch(console.error);
     if(!user) return message.reply("Couldn't find that user...");
@@ -18,7 +18,7 @@ exports.run = (client, message, args) => {
     .addField('Action:', 'Warn Check')
     .addField('User:', `${user.username}#${user.discriminator}`)
     .addField('Number of warnings:', warns[`${user.id}, ${message.guild.id}`].warns)
-    .setFooter(`© giANTbot by ${customisation.ownername}`);
+    .setFooter(`© Cryptonix X Mod Bot by ${customisation.ownername}`);
     message.channel.send({embed});
 }
 
