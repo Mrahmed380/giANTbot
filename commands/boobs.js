ff504bb5782d32b9462855decb4a5d0ae2c4da4b
GiCl90 <65637325+GiCl90@users.noreply.github.com>
2020-05-24 16:58:30 +0200

    Update boobs.js

--- commands/boobs.js
+++ commands/boobs.js
@@ -4,13 +4,11 @@ const customisation = require('../customisation.json');
 
 exports.run = async (client, message, args, tools) => {
     if(!message.channel.nsfw) return message.reply("NSFW is not enabled in this channel");
-    const { body } = await superagent
-    .get("https://cdn.imagefap.com/images/full/44/765/765304600.jpg?end=1590418224&secure=04c2c3ec08e3c0d677db4");
     
     const embed = new Discord.MessageEmbed()
     .setColor("#ff9900")
     .setTitle(`take some boobies`)
-    .setImage(body.url) 
+    .setImage(`${boobs[Math.floor(Math.random() * boobs.length)]}`) 
     .setFooter(`© giANTbot by ${customisation.ownername}`);
     message.channel.send({embed})
 };
