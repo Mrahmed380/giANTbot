628c8630a8c6fd16d6cc73241e77e795639cf2a1
GiCl90 <65637325+GiCl90@users.noreply.github.com>
2020-05-24 23:10:04 +0200

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -6,37 +6,37 @@ exports.run = (client, message, args) => {
   if (message.mentions.users.size < 1) return message.reply('You must mention someone to kick them.').catch(console.error);
   if (user.id === message.author.id) return message.reply("I can't let you do that, self-harm is bad:facepalm:");
   if (user.id === client.user.id) return message.reply("You pleblord, how can you use a bot to kick itself?:joy:");
-  
+
   if (message.mentions.users.first().id === "434434883314647063") return message.reply("You can't kick my Developer:wink:");
   if (reason.length < 1) reason = 'No reason supplied';
 
   if (!message.guild.member(user).kickable) return message.reply('I cannot kick that member');
-  
+
   const embed = new Discord.MessageEmbed()
-  .setColor(0x0000FF)
-  .setTimestamp()
-  .addField('Action:', 'Kick')
-  .addField('User:', `${user.username}#${user.discriminator} (${user.id})`)
-  .addField('Moderator:', `${message.author.username}#${message.author.discriminator}`)
-  .addField('Reason', reason)
-  .setFooter(`© giANTbot by ${customisation.ownername}`);
-  
-  if(user.bot) return;
-   message.mentions.users.first().send({embed}).catch(e =>{
-    if(e) return
+    .setColor(0x0000FF)
+    .setTimestamp()
+    .addField('Action:', 'Kick')
+    .addField('User:', `${user.username}#${user.discriminator} (${user.id})`)
+    .addField('Moderator:', `${message.author.username}#${message.author.discriminator}`)
+    .addField('Reason', reason)
+    .setFooter(`© giANTbot by ${customisation.ownername}`);
+
+  if (user.bot) return;
+  message.mentions.users.first().send({ embed }).catch(e => {
+    if (e) return
   });
   message.guild.member(user).kick();
 
   let logchannel = message.guild.channels.find('name', 'logs');
-  if  (!logchannel){
-  message.channel.send({embed})
-  }else{
-    client.channels.get(logchannel.id).send({embed});
-    message.channel.send({embed})
-  } 
-  if(user.bot) return;
-  message.mentions.users.first().send({embed}).catch(e =>{
-    if(e) return 
+  if (!logchannel) {
+    message.channel.send({ embed })
+  } else {
+    client.channels.get(logchannel.id).send({ embed });
+    message.channel.send({ embed })
+  }
+  if (user.bot) return;
+  message.mentions.users.first().send({ embed }).catch(e => {
+    if (e) return
   });
 };
 
