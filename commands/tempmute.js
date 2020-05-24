5999aa9ebba43d0854b1671cdaf6abbd295da95e
GiCl90 <65637325+GiCl90@users.noreply.github.com>
2020-05-24 22:52:29 +0200

    Update tempmute.js

--- commands/tempmute.js
+++ commands/tempmute.js
@@ -45,7 +45,6 @@ exports.run = async (client, message, args) => {
         if (e) return
     });
 
-    tomute.removeRoles(tomute.roles).then(console.log).catch(console.error);
     message.guild.member(tomute).roles.add(muteRole);
 
     setTimeout(function () {
