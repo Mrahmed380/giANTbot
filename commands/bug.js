43a35fb5f93ff12b5285722d3111e9d1682945ae
GiCl90 <65637325+GiCl90@users.noreply.github.com>
2020-05-24 15:59:47 +0200

    Update bug.js

--- commands/bug.js
+++ commands/bug.js
@@ -4,7 +4,7 @@ exports.run = (client, message, args) => {
     if (!args[0]) return message.reply("Please specify the bug. Example:\n`/punch isn't working. It isn't mentioning the user I'm trying to punch`");
     if (args[0] === "bug") return message.reply("Please specify the bug. Example:\n`/punch isn't working. It isn't mentioning the user I'm trying to punch`");
     args = args.join(" ");
-    message.reply("Thanks for submitting a bug! <a:balancecheck:556017659419033653>");
+    message.reply("Thanks for submitting a bug!");
     const content = `**${message.author.username}#${message.author.discriminator}** (${message.author.id}) reported:\n~~--------------------------------~~\n${args}\n~~--------------------------------~~\nOn the server: **${message.guild.name}**\nServer ID: **${message.guild.id}**`;
     client.channels.cache.get(customisation.bugchannelid).send(content)
 }
