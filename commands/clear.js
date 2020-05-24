f720802fbaf4bbf64d661b0f7e4e8557f75b4b7a
GiCl90 <65637325+GiCl90@users.noreply.github.com>
2020-05-24 17:45:05 +0200

    Create clear.js

---
+++ commands/clear.js
@@ -0,0 +1,50 @@
+const Discord = require("discord.js");
+const customisation = require('../customisation.json');
+
+exports.run = async (bot, message, args) => {
+
+    // !clear aantal
+    if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Je hebt geen toestemming");
+
+    if (!args[0]) return message.reply("Geef een aantal op dat je weg wilt halen");
+
+    if (Number.isInteger(parseInt(args[0]))) {
+
+        var aantal = parseInt(args[0]) + 1;
+
+        message.channel.bulkDelete(aantal).then(() => { 
+
+            if (args[0] == 0) {
+
+                message.reply(`Ben je loemp ik kan toch niet 0 berichten verwijderen?`).then(msg => msg.delete({timeout: 3000}));
+            
+            } else if (args[0] == 1) {
+            
+                message.reply(`Ik heb 1 bericht verwijderd.`).then(msg => msg.delete({timeout: 3000}));
+            
+            } else {
+            
+                message.reply(`Ik heb ${args[0]} berichten verwijderd.`).then(msg => msg.delete({timeout: 3000}));
+            
+            }
+
+        });
+
+    } else {
+        return message.reply("Geef een getal op.");
+    }
+
+}
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: [],
+    permLevel: 0
+  };
+  
+exports.help = {
+    name: 'whosgay',
+    description: 'Ask the bot who is gay',
+    usage: 'whosgay'
+  };
+  
