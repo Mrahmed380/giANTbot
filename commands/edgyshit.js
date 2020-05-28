fecc0a01e02c490bf65b199bb88741fa11a18953
GiCl90 <65637325+GiCl90@users.noreply.github.com>
2020-05-29 00:20:51 +0200

    yep

--- commands/edgyshit.js
+++ commands/edgyshit.js
@@ -1,4 +1,3 @@
-const copypastas = require('../data/copypastas.json');
 exports.run = (client, message, args) => {
     args = args.join(" ");
     message.channel.send(`:syringe::knife: :syringe::knife::syringe::knife:edgy shit edgY sHit :knife:thats :gun:some edgy:syringe::syringe: shit right :knife:th:knife: ere:syringe::syringe::syringe: right there :smoking::smoking:if i do ƽaү so my selｆ :gun:i say so :gun: thats what im talking about right there right there (chorus: ʳᶦᵍʰᵗ ᵗʰᵉʳᵉ) mMMMMᎷМ:gun: :knife::knife::knife:НO0ОଠＯOOＯOОଠଠOoooᵒᵒᵒᵒᵒᵒᵒᵒᵒ:knife::knife::knife: :gun: :syringe::syringe: :knife::knife: Edgy shit`);
