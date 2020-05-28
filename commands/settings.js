77d4373eaaf05609360657b4384597930cbbcefc
GiCl90 <65637325+GiCl90@users.noreply.github.com>
2020-05-28 19:26:02 +0200

    Update settings.js

--- commands/settings.js
+++ commands/settings.js
@@ -5,8 +5,8 @@ const fs = require('fs')
 const mongoose = require('mongoose')
 
 exports.run = async (client, message, args) => {
-    if (!message.member.hasPermission("ADMINISTRATOR") && message.author.id !== '434434883314647063') return message.reply("❌**Error:** You have to be an Admin to use this command!");
-    if (message.author.id == '434434883314647063' || message.member.hasPermission("ADMINISTRATOR"))
+    // if (!message.member.hasPermission("ADMINISTRATOR") && message.author.id !== '434434883314647063') return message.reply("❌**Error:** You have to be an Admin to use this command!");
+    // if (message.author.id == '434434883314647063' || message.member.hasPermission("ADMINISTRATOR"))
     if(!args) return message.reply("Usage: `settings current|example|template|upload (file upload)`")
     if(args[0] === 'template'){
         message.channel.send("Here's the settings template.", { files: ['./temp/thing.json'] });
