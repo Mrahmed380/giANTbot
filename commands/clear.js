c156fb898c4c16bd9bb93d0857a885140113a6b1
GiCl90 <65637325+GiCl90@users.noreply.github.com>
2020-05-28 17:07:59 +0200

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -1,44 +1,46 @@
 const Discord = require("discord.js");
+
 const customisation = require('../customisation.json');
 
 exports.run = async (bot, message, args) => {
 
     // !clear aantal
+
     if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("You dont have permission for this command!");
 
     if (!message.guild.me.hasPermission("MANAGE_MESSAGES")) return message.reply("I dont have the permissions to do this");
 
     if (message.author.id == '434434883314647063' || message.member.hasPermission("MANAGE_MESSAGES"));
 
-    if (!args[0]) return message.reply("Specify the number of messages") {
+    if (!args[0]) return message.reply("Specify the number of messages");
 
     if (Number.isInteger(parseInt(args[0]))) {
 
         var aantal = parseInt(args[0]) + 1;
 
-        message.channel.bulkDelete(aantal).then(() => { 
+        message.channel.bulkDelete(aantal).then(() => {
 
             if (args[0] == 0) {
 
-                message.reply(`Cant delete 0 messages.`).then(msg => msg.delete({timeout: 3000}));
-            
+                message.reply(`Cant delete 0 messages.`).then(msg => msg.delete({ timeout: 3000 }));
+
             } else if (args[0] == 1) {
-            
-                message.reply(`Deleted 1 message!`).then(msg => msg.delete({timeout: 3000}));
-            
+
+                message.reply(`Deleted 1 message!`).then(msg => msg.delete({ timeout: 3000 }));
+
             } else {
-            
-                message.reply(`Deleted ${args[0]} messages!`).then(msg => msg.delete({timeout: 3000}));
-            
+
+                message.reply(`Deleted ${args[0]} messages!`).then(msg => msg.delete({ timeout: 3000 }));
+
             }
 
         });
 
     } else {
-        return message.reply("Specify a number!");
-    }
 
-};
+        return message.reply("Specify a number!");
+
+    }
 
 }
 exports.conf = {
@@ -46,11 +48,11 @@ exports.conf = {
     guildOnly: false,
     aliases: [],
     permLevel: 0
-  };
-  
+};
+
 exports.help = {
     name: 'clear',
     description: 'Clear messages',
     usage: 'clear #'
-  };
-  
+};
+
