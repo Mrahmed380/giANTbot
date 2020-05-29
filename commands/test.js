3e856c0955cbd8a08b8bc3f8d048c15966c09a3f
GiCl90 <65637325+GiCl90@users.noreply.github.com>
2020-05-29 12:24:25 +0200

    Create test.js

---
+++ commands/test.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js");
+const customisation = require('../customisation.json');
+
+exports.run = async (bot, message, args) => {
+
+    var user = message.guild.members.random();
+    console.log(`${user.user}`);
+
+}
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: [],
+    permLevel: 0
+  };
+  
+exports.help = {
+    name: 'test',
+    description: 'Ask the bot who is gay',
+    usage: 'test'
+  };
+  
